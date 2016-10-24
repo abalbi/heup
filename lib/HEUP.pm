de1ee3347b1607e0ecf570a65996b245f166f8a2
Alejandro Balbi <alejandrobalbi@gmail.com>
2016-10-24 11:19:54 -0300

    Revert "Commit temporal"
    This reverts commit f43494666fbfb08073e93f95da2564437d4a95e0.

--- lib/HEUP.pm
+++ lib/HEUP.pm
@@ -2,24 +2,14 @@ package HEUP;
 use Data::Dumper;
 use lib 'lib';
 
-use Model::Personaje;
-use Model::Atributo;
-use Service::Atributo;
-use Service::Personaje;
-
 our $app_path = $ENV{PWD};
 our $bin_path = $app_path.'/bin';
 our $heup_path = $bin_path.'/heup';
 
 chmod 0755, $heup_path if ! -X $heup_path;
 
-srand(24170985);
-
 sub ejecutar {
-	my $class = shift;
-	my $args = {@_};
-	my $personaje = Service::Personaje->crear($args);
-	return $personaje->detalle;
+	return ':';
 }
 
 1;
